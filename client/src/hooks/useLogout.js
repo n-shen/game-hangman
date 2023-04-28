4388db895061ed10342b56b21e490473ada739e0
Nicholas Shen <tk@shenn.me>
2023-04-28 01:03:57 -0700

    game: fixed data reset during user change

--- client/src/hooks/useLogout.js
+++ client/src/hooks/useLogout.js
@@ -1,9 +1,24 @@
 import { useAuthContext } from "./useAuthContext";
+import { useStateContext } from "../contexts/StateContext";
 
 export const useLogout = () => {
   const { dispatch } = useAuthContext();
+  const {
+    setNewRound,
+    setCurrScore,
+    setCurrWinner,
+    setCurrEasy,
+    setCurrNormal,
+    setCurrHard,
+  } = useStateContext();
 
   const logout = () => {
+    setNewRound(true);
+    setCurrScore(0);
+    setCurrWinner(false);
+    setCurrEasy(0);
+    setCurrNormal(0);
+    setCurrHard(0);
     localStorage.removeItem("token");
     dispatch({ type: "LOGOUT" });
   };
