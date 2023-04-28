fb607c50bbfc71761c824774a5de08f056d984fb
Nicholas Shen <tk@shenn.me>
2023-04-28 12:00:58 -0700

    Overall: added account deletion

--- server/routes/profileRouter.js
+++ server/routes/profileRouter.js
@@ -4,6 +4,7 @@ import validAuth from "../middleware/validAuth.js";
 import {
   getProfile,
   updateProfile,
+  destroyProfile,
 } from "../controllers/profile.controller.js";
 
 const profileRouter = express.Router();
@@ -11,5 +12,6 @@ const profileRouter = express.Router();
 profileRouter.use(validAuth);
 profileRouter.post("/get", getProfile);
 profileRouter.post("/update", updateProfile);
+profileRouter.post("/destroy", destroyProfile);
 
 export default profileRouter;
